9eaf5102a787a36c7a7e30e89726b4059b2ee668
Diego-ARE <darevaloh1@miumg.edu.gt>
2024-10-24 01:43:42 -0600

    a

--- server.js
+++ server.js
@@ -13,10 +13,15 @@ app.post('/send-email', async (req, res) => {
     try {
         const { name, email, subject, message } = req.body;
 
+        // Validación básica
+        if (!name || !email || !subject || !message) {
+            return res.status(400).send({ message: 'Todos los campos son requeridos.' });
+        }
+
         const emailData = {
             personalizations: [
                 {
-                    to: [{ email: 'darevaloh1@miumg.edu.gt' }],
+                    to: [{ email: 'darevaloh1@miumg.edu' }],
                     subject: `Nuevo caso de contacto: ${subject}`,
                 },
             ],
@@ -31,16 +36,18 @@ app.post('/send-email', async (req, res) => {
 
         const response = await axios.post('https://api.sendgrid.com/v3/mail/send', emailData, {
             headers: {
-                'Authorization': `Bearer ${process.env.SENDGRID_API_KEY}`, // Cambia esto
+                'Authorization': `Bearer ${process.env.SENDGRID_API_KEY}`,
                 'Content-Type': 'application/json',
             },
         });
 
         res.status(response.status).send(response.data);
     } catch (error) {
+        console.error('Error al enviar el correo:', error);
         res.status(500).send({ message: 'Error al enviar el correo', error: error.message });
     }
 });
+
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
